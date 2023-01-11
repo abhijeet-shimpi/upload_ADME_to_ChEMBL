8180662cf08ff2f5c4d2e5fa14d2231563a9b722
abhijeet-shimpi <117220508+abhijeet-shimpi@users.noreply.github.com>
2023-01-11 14:49:15 +0000

    App for ETL operations on TDC ADME data. ETL operations include extracting ADME data from TDC, splitting data into test, train and validation sets. creating pandas dataframe for the split data and adding few more meta data. Loading this data to the ChEMBL database.

--- app.py
+++ app.py
@@ -7,23 +7,35 @@ from datetime import datetime
 
 class ETL:
 
-    def __init__(self):
+    def __init__(self, name, adme_type):
         self.data_df = pd.DataFrame()
+        self.name = name
+        self.adme_type = adme_type
+        self.split = dict()
+        self.df = pd.DataFrame()
 
     def extract(self):
-        name = 'BBB_Martins'
-        adme_type = 'Distribution'
-        data = ADME(name=name)
-        # split = data.get_split()
-        df = data.get_data(format='df')
-        self.data_df = self.data_df.append(df)
-        self.data_df['adme_type'] = adme_type
-        self.data_df['drug_type'] = name
-        self.data_df['date_inserted'] = datetime.now()
-        print(self.data_df)
+
+        print(f'Data extraction started for {self.adme_type}:{self.name}')
+        data = ADME(name=self.name)
+        self.split = data.get_split()
+        print(self.split)
+        # df = data.get_data(format='df')
+        # self.data_df = self.data_df.append(df)
+
 
     def transform(self):
-        pass
+        print(f'Data tranforming for {self.adme_type}:{self.name}')
+
+        for key in self.split:
+            dataset = pd.DataFrame(self.split[key])
+            dataset['dataset_type'] = key
+            self.data_df = self.data_df.append(dataset)
+
+        self.data_df['adme_type'] = self.adme_type
+        self.data_df['drug_type'] = self.name
+        self.data_df['date_inserted'] = str(datetime.now()).split('.')[0]
+        self.data_df['Drug_ID'] = self.data_df['Drug_ID'].str.replace('\'', '\'\'')
 
     def validation(self):
         pass
@@ -31,12 +43,21 @@ class ETL:
     def load(self):
         pg_connection_detail = utils.config('pg_db')
         config = dict(pg_connection_detail= pg_connection_detail,
-                      model = model.ADME)
+                      model = model.ADME,
+                      adme_type = self.adme_type)
         conn = Pgsql(config)
-        conn.insert_pd_df(self.data_df)
-        print('Data inserted successfull!')
+        conn.insert_data(self.data_df)
+
 
 
 if __name__  == "__main__":
-    ETL().extract()
-    ETL().load()
+    adme = dict(
+        Distribution = ['BBB_Martins'],
+        Absorption = ['Caco2_Wang']
+    )
+    for adme_type in adme:
+        for name in adme[adme_type]:
+            etl = ETL(name, adme_type)
+            etl.extract()
+            etl.transform()
+            etl.load()
