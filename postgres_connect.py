b8916416085b08156846ef1685974f14b3242896
abhijeet-shimpi <117220508+abhijeet-shimpi@users.noreply.github.com>
2023-01-12 12:31:13 +0000

    renamed and added comments

--- postgres_connect.py
+++ postgres_connect.py
@@ -3,7 +3,7 @@
 import psycopg2
 import psycopg2.extras as extras
 
-
+# Defining datatypes in postgres db
 DEFAULT_DATATYPES = dict(
     str = 'text',
     int = 'int',
@@ -17,10 +17,6 @@ class Pgsql:
     def __init__(self, config: dict()):
         self.connection_details = config.get('pg_connection_detail')
         self.cnn = self.connection()
-        self.host = self.connection_details.get('host')
-        self.user = self.connection_details.get('user')
-        self.database = self.connection_details.get('database')
-        self.password = self.connection_details.get('password')
         self.cursor = self.cnn.cursor()
         self.adme_type = config.get('adme_type')
         self.model = config.get('model')
@@ -32,6 +28,7 @@ class Pgsql:
         self.create_table_if_not_exist()
 
     def connection(self):
+        # connect to postgres instance of Chembl and return psycopg connection object
         self.cnn = psycopg2.connect(**self.connection_details)
         print('Connected to PostgreSQL')
         return self.cnn
@@ -40,10 +37,12 @@ class Pgsql:
         self.cnn.close()
 
     def create_schema_if_not_exist(self):
+        # create schema for first instance
         qry = f"""CREATE SCHEMA IF NOT EXISTS {self.model.schema};"""
         self.cursor.execute(qry)
 
     def create_table_if_not_exist(self):
+        # create table for first instance
         qry = f"""
                 CREATE TABLE IF NOT EXISTS {self.model.schema}.{self.table_name}
                 ({self.text_column})
@@ -51,6 +50,7 @@ class Pgsql:
         self.cursor.execute(qry)
 
     def model_columns(self):
+        #resolving dataclass fields into different formats
         self.col_names = list(self.model.__dataclass_fields__.keys())
 
         self.col_types = dict()
@@ -68,10 +68,14 @@ class Pgsql:
         self.commit()
 
     def bulk_execute(self, qry, values):
+        # Function executes the insert query on to Chembl db
         extras.execute_values(self.cursor, qry, values)
         self.commit()
 
     def insert_data(self, data):
+        #convert split dataframe rows into list of tuples before bulk insertion process
+        #build insert query and passing to bulk execute function
+
         # tuples = [tuple(row) for index, row in data.iterrows()]
         values = list(data.itertuples(index=False, name=None))
         qry = f'''INSERT INTO {self.model.schema}.{self.table_name} ({','.join(self.col_names)}) VALUES %s'''
@@ -86,6 +90,7 @@ class Pgsql:
         self.cnn.commit()
 
     def read_from_cursor(self):
+        #to read output of a query from Chembl db
         cols = [col_def[0] for col_def in self.cursor.description]
         rows = self.cursor.fetchall()
         return cols, rows
